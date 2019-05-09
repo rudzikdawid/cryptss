c1cbb79dec87050e8394590f0c4f6c00d39561a1
dawid.rudzik <dawid.rudzik@polchip.pl>
2019-05-09 21:20:50 +0200

    async pipe zamiast subscribe

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { filter, map, pairwise, startWith } from 'rxjs/operators';
 import { WebsocketService } from '../websocket/websocket.service';
 import * as fromWorkspace from './state/workspace.reducer';
@@ -11,10 +11,6 @@ import { WorkspaceService } from './workspace.service';
 import { myAnimation, fadeAnimation, slideInAnimation } from './animations';
 
 const nestedViews = ['add-conversation', 'detail-conversation'];
-const forwardRotation = {
-    leaveDeg: 180,
-    enterDeg: -180
-};
 
 @Component({
   selector: 'app-workspace',
@@ -38,39 +34,34 @@ export class WorkspaceComponent implements OnInit {
 
   user$: Observable<User>;
   wsStatus$ = this.websocketService.connectionStatus$;
-  params;
   routeChange$;
 
   ngOnInit() {
+      const initNavigationEndEvent = new NavigationEnd(null, this.router.url, this.router.url);
+
       this.routeChange$ = this.router.events
           .pipe(
-              startWith(new NavigationEnd(null, this.router.url, this.router.url)),
               filter(e => e instanceof NavigationEnd),
+              startWith(initNavigationEndEvent, initNavigationEndEvent),
               pairwise(),
               map(([prevRoute, nextRoute] : [NavigationEnd, NavigationEnd]) => {
-                  return nestedViews.find(elm => nextRoute.url.includes(elm)) ?
-                      forwardRotation : {
-                          leaveDeg: -180,
-                          enterDeg: 180
-                      };
+                  return {
+                      value: nextRoute.url,
+                      params: nestedViews.find(elm => nextRoute.url.includes(elm)) ?
+                          {
+                              leaveDeg: -180,
+                              enterDeg: 180
+                          } : {
+                              leaveDeg: 180,
+                              enterDeg: -180
+                          }
+                  }
               })
-          )
-          .subscribe((next) => {
-              this.params = next;
-          });
+          );
 
       this.store.dispatch(new workspaceActions.Load());
       this.user$ = this.store.pipe(select(fromWorkspace.userWorkspace));
       this.workspaceService.setLeftSidenavRef(this.leftSidenavRef);
-  }
-
-  prepareRoute(outlet: RouterOutlet) {
-    const uuu = outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
-
-    return {
-        value: uuu,
-        params: this.params || forwardRotation
-    };
   }
 
 }
